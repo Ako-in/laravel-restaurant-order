bc25670baee52fef95bebb7872abd798e0aa195a
Ako-in <sinba.805i@gmail.com>
2025-08-03 10:08:17 +0900

    コミットtest

--- app/Http/Controllers/Customer/AuthController.php
+++ app/Http/Controllers/Customer/AuthController.php
@@ -6,11 +6,13 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\Log; 
 
 class AuthController extends Controller
 {
     public function showLoginForm()
     {
+        Log::info('ログ確認: ' . now()); 
         return view('customer.login');
     }
 
