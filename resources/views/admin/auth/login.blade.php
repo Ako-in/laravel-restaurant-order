89c60781fe8d1fdaac21c5d4fa24141b0b992a64
Ako-in <sinba.805i@gmail.com>
2025-08-11 19:00:18 +0900

    管理者ログイン画面navメニュー非表示

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -1,6 +1,59 @@
-@extends('layouts.admin')
+<!DOCTYPE html>
+<html lang="ja">
 
-@section('content')
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    {{-- <title>{{ config('app.name', 'Laravel') }}</title> --}}
+    <title>レストラン注文アプリ-管理画面</title>
+
+    {{-- Flatpickr CSS --}}
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
+
+    {{-- まだ使用していない --}}
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <style>
+        header {
+            position: fixed;
+            top: 0;
+            width: 100%;
+            height: 60px;
+            background-color: white;
+            z-index: 1000;
+            box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+        }
+
+        footer {
+            position: fixed;
+            bottom: 0;
+            width: 100%;
+            height: 50px;
+            background-color: white;
+            z-index: 1000;
+            box-shadow: 0 -2px 5px rgba(0, 0, 0, 0.1);
+        }
+
+        body {
+            padding-top: 60px;
+            padding-bottom: 50px;
+        }
+    </style>
+</head>
+
+<header>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            {{-- タイトル（左寄せ） --}}
+            <a href="{{ route('admin.home') }}" class="navbar-brand">Urban Spoon管理画面</a>
+        </div>
+    </nav>
+</header>
+<body>
     <div class="container my-4">
         <div class="row justify-content-center">
             <div class="col-xl-3 col-lg-4 col-md-5 col-sm-7">
@@ -46,5 +99,9 @@
                 </form>
             </div>
         </div>
-    </div>
-@endsection
+    </div>    
+</body>
+
+
+
+
